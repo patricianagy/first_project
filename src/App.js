783941058dc78c09e892ac7d9126440fc67719ea
patricianagy <nagy.patricia98@gmail.com>
2021-05-04 15:08:11 +0200

    Created hero component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg'
 import './App.scss'
 import Button from 'react-bootstrap/Button'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Hero from './components/hero.js'
 
 function App() {
   return (
@@ -13,18 +13,7 @@ function App() {
         <Route exact path="/">
           <div className="App">
             <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
+              <Hero />
               <Link to="/first-router">
                 <Button variant="outline-success">Lovely succes button</Button>
               </Link>
---
+++ src/components/hero.js
@@ -0,0 +1,14 @@
+import './hero.scss'
+
+function Hero() {
+  return (
+    <div>
+      <header class="Hero-header">
+        <h1>First App</h1>
+        <h5>First App just in smaller</h5>
+      </header>
+    </div>
+  )
+}
+
+export default Hero
