1524ad748f90d24a4d52f7eb115a45fb1da9c8dd
patricianagy <nagy.patricia98@gmail.com>
2021-05-04 13:54:05 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg'
 import './App.scss'
+import Button from 'react-bootstrap/Button'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Switch>
+        <Route path="/first-router">
+          <First />
+        </Route>
+        <Route exact path="/">
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+              <Link to="/first-router">
+                <Button variant="outline-success">Lovely succes button</Button>
+              </Link>
+            </header>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+function First() {
+  return (
+    <div>
+      <h3>🍉</h3>
+      <Link to="/">
+        <Button variant="outline-success">Back</Button>
+      </Link>
     </div>
   )
 }
