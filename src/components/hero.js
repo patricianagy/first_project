d90fd37fbf404eb7520d8587c34be955c9773653
patricianagy <nagy.patricia98@gmail.com>
2021-05-10 10:37:49 +0200

    Updated hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,32 +3,47 @@ import Contaiener from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
+import hero from './hero.jpg'
 
 function Hero() {
   return (
     <div>
-      <Contaiener fluid className="Container">
+      <Contaiener className="ml-6">
         <Row>
-          <Col md={5} className="l-3">
-            <h1 className="Heading">
-              Many reasons to get up and start to get back in business
-            </h1>
+          <Col lg={7}>
+            <Contaiener fluid className="Container">
+              <Row>
+                <Col>
+                  <h1 className="Heading">
+                    Many reasons to get up and start to get back in business
+                  </h1>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <h5 className="Subheading">
+                    The harder you work for something, the greater you'll feel
+                    when you achive it.
+                  </h5>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Button variant="learn-more">Learn more</Button>
+                  <Button variant="demo">Demo</Button>
+                </Col>
+              </Row>
+            </Contaiener>
           </Col>
-        </Row>
-        <Row>
-          <Col md={4} className="l-3">
-            <h5 className="Subheading">
-              The harder you work for something, the greater you'll feel when
-              you achive it.
-            </h5>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={2} className="l-3">
-            <Button variant="learn-more">Learn more</Button>
-          </Col>
-          <Col className="l-2">
-            <Button variant="learn-more">Learn more</Button>
+          <Col lg={5}>
+            <Contaiener fluid>
+              <Row>
+                <Col lg={{ span: 12 }} className="mt-6">
+                  <Image src={hero} alt="Hero" rounded fluid />
+                </Col>
+              </Row>
+            </Contaiener>
           </Col>
         </Row>
       </Contaiener>
