b8220a9528b57b6ddd6371a16401a99238b42300
Judit Bauer <judit.bauer@yahoo.com>
2021-05-11 09:07:59 +0200

    Refactor hero section.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
-import './App.scss'
-import Button from 'react-bootstrap/Button'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Hero from './components/hero.js'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/first-router">
-          <First />
-        </Route>
-        <Route exact path="/">
-          <div className="App">
-            <header className="App-header">
-              <Hero />
-              {/*<Link to="/first-router">
-                <Button variant="outline-success">Lovely succes button</Button>
-              </Link>*/}
-            </header>
-          </div>
-        </Route>
-      </Switch>
-    </Router>
-  )
-}
-
-function First() {
-  return (
-    <div>
-      <h3>🍉</h3>
-      <Link to="/">
-        <Button variant="outline-success">Back</Button>
-      </Link>
+    <div className="app">
+      <Hero />
     </div>
   )
 }
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,80 +1,64 @@
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Image,
+  ResponsiveEmbed,
+} from 'react-bootstrap'
+import hero from '../assets/hero.jpg'
 import './hero.scss'
-import Contaiener from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
-import Image from 'react-bootstrap/Image'
-import hero from './hero.jpg'
-import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
 
 function Hero() {
   return (
-    <div>
-      <Contaiener fluid>
-        <Row>
-          <Col md={5}>
-            <Contaiener fluid className="ml-6">
-              <Row>
-                <Col>
-                  <h1 className="Heading">
-                    Many reasons to get up and start to get back in business
-                  </h1>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <h5 className="Subheading">
-                    The harder you work for something, the greater you'll feel
-                    when you achive it.
-                  </h5>
-                </Col>
-              </Row>
-              <Row>
-                <Col md={4}>
-                  <Button variant="learn-more" block>
-                    Learn more
-                  </Button>
-                </Col>
-                <Col md={4}>
-                  <Button variant="demo" block>
-                    Demo
-                  </Button>
-                </Col>
-              </Row>
-              <Row className="my-5">
-                <Col md={4}>
-                  <ResponsiveEmbed aspectRatio="16by9">
-                    <iframe
-                      src="https://www.youtube.com/embed/7yLxxyzGiko"
-                      frameBorder="0"
-                      allow="autoplay"
-                      allowFullScreen
-                      title="video"
-                    />
-                  </ResponsiveEmbed>
-                </Col>
-                <Col>
-                  <p className="basic-text">
-                    The harder you work for something, the greater you’ll feel
-                    when you achieve it.
-                  </p>
-                  <p className="link-text">Watch preview</p>
-                </Col>
-              </Row>
-            </Contaiener>
-          </Col>
-          <Col md={6}>
-            <Contaiener fluid>
-              <Row>
-                <Col lg={{ span: 12 }} className="mt-6">
-                  <Image src={hero} alt="Hero" rounded fluid />
-                </Col>
-              </Row>
-            </Contaiener>
-          </Col>
-        </Row>
-      </Contaiener>
-    </div>
+    <Container>
+      <Row>
+        <Col md={6}>
+          <h1 className="heading">
+            Many reasons to get up and start to get back in business
+          </h1>
+          <h5 className="subheading">
+            The harder you work for something, the greater you'll feel when you
+            achive it.
+          </h5>
+          <Row>
+            <Col md={4}>
+              <Button variant="button" block>
+                Learn more
+              </Button>
+            </Col>
+            <Col md={4}>
+              <Button variant="outline-button" className="btn-demo" block>
+                Demo
+              </Button>
+            </Col>
+          </Row>
+          <Row className="my-5">
+            <Col md={4}>
+              <ResponsiveEmbed aspectRatio="16by9">
+                <iframe
+                  src="https://www.youtube.com/embed/7yLxxyzGiko"
+                  frameBorder="0"
+                  allow="autoplay"
+                  allowFullScreen
+                  title="video"
+                />
+              </ResponsiveEmbed>
+            </Col>
+            <Col>
+              <p className="basic-text">
+                The harder you work for something, the greater you’ll feel when
+                you achieve it.
+              </p>
+              <p className="link-text">Watch preview</p>
+            </Col>
+          </Row>
+        </Col>
+        <Col md={6}>
+          <Image src={hero} alt="hero" rounded fluid />
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
