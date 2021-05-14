139f6b81ee0210c18b7be7c7f3a8835d084acb2e
patricianagy <nagy.patricia98@gmail.com>
2021-05-14 14:04:28 +0200

    Created counter component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Hero from './components/hero/hero.js'
 import NavigationBar from './components/navigationBar/navigationBar.js'
 import Footer from './components/footer/footer.js'
+import Counter from './components/counter/counter.js'
 
 function App() {
   return (
     <div className="app px-5 py-3">
       <NavigationBar />
       <Hero />
+      <Counter />
       <Footer />
     </div>
   )
